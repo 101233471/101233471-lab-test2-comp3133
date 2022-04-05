26b2b1e50396838fab9e52d691f06af33c55e412
101233471 <tosellonicole2001@gmail.com>
2022-04-05 12:41:12 -0400

    adding missionlist

---
+++ src/app/missionlist/missionlist.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: 'app-missionlist',
+  templateUrl: './missionlist.component.html',
+  styleUrls: ['./missionlist.component.css']
+})
+export class MissionlistComponent implements OnInit {
+  data:any;
+  constructor(private postData:PostsService) { }
+
+  ngOnInit(): void {
+    this.postData.getPosts().subscribe((result)=>{
+      console.log("result",result)
+      this.data=result
+    })
+  }
+
+}
---
+++ src/app/posts.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+  url="https://api.spacexdata.com/v3/launches"
+
+  constructor(private http:HttpClient) { }
+  
+  getPosts(){
+    return this.http.get(this.url);
+  }
+}
